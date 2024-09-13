600cecdc15d20a541484cc6a535b198cc158fbb8
Lucascg <lucascgiacomin16@gmail.com>
2024-09-13 00:29:48 -0300

    estilizando a tela de login

--- ServicoInWeb/Models/ViewModels/LoginViewModel.cs
+++ ServicoInWeb/Models/ViewModels/LoginViewModel.cs
@@ -5,6 +5,7 @@ namespace ServicoInWeb.Models
     public record LoginViewModel
     {
         [Required(ErrorMessage = "O campo e-mail é obrigatório")]
+        [EmailAddress(ErrorMessage = "O formato do e-mail digitado é inválido")]
         public string Email { get; set; } = string.Empty;
         [Required(ErrorMessage = "O campo senha é obrigatório")]
         public string Password { get; set; } = string.Empty;
