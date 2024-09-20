4deda713c8ba73f21232770ae08a3c6d672cbdab
Lucascg <lucascgiacomin16@gmail.com>
2024-09-20 12:04:38 -0300

    Criando as paginas do sistema

---
+++ ServicoInWeb/Controllers/ServicoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServicoInWeb.Controllers
+{
+    public class ServicoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
